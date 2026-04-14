cbbce9d41b9e04fac62d804bfb10a310124f2d0b
Tyler  Lemke <tyleris@gmail.com>
2026-04-14 16:15:23 -0700

    Create functions.js

---
+++ FS12-Week-5/Session-01/01-functions/functions.js
@@ -0,0 +1,14 @@
+// define the function
+// here is what your function does
+// use action words for function name
+function openDoor(){
+    //your code goes in here
+    return "You opened the door";
+}
+
+// call, invoke, execute, run
+console.log(openDoor())
+
+openDoor // this doesn't do anything we can see
+
+// whats the difference - not counting console.log() - between lines 10 and 12
