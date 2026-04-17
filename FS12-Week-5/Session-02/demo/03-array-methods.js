dd64e751031a4fca1674fd2b8c4a5d4896cd90ab
Tyler  Lemke <tyleris@gmail.com>
2026-04-16 17:08:19 -0700

    arrays, index, array methods framework start

---
+++ FS12-Week-5/Session-02/demo/03-array-methods.js
@@ -0,0 +1,11 @@
+// array methods are just functions associated with the array data type
+
+const fruits = ["banana", "kiwi", "cherry", "apple", "strawberry"];
+
+// type in "fruits." in VSCODE
+// take a look at all of the methods available to you
+// if you were to guess, what do they do?
+
+//fruits.
+
+
