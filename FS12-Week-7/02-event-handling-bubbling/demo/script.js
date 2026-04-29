21ba096fb67632280ff6f4aa1cbbee31be10fcfe
Tyler  Lemke <tyleris@gmail.com>
2026-04-28 17:29:00 -0700

    1st event listener

--- FS12-Week-7/02-event-handling-bubbling/demo/script.js
+++ FS12-Week-7/02-event-handling-bubbling/demo/script.js
@@ -1,8 +1,21 @@
 console.log("hello world");
 
-// Click EVENT START
-// alert the user with a "hello"
+// Click EVENT START of Code
 
+// goal
+// alert the user with a "hello"
 // when they click on the hello button
 
 // select the hello button
+const helloBtn = document.querySelector("#hello");
+
+// addEventListener - needs the type of event and the
+// callback function
+
+function handleHelloClick(event) {
+  console.log("Hello handler was called");
+  console.log("event", event);
+  alert("Hey, how are you doing?!?!?");
+}
+
+helloBtn.addEventListener("click", handleHelloClick)
