fe9acf4e6035d8ae9657b57b08e20c533b6fb7d0
Tyler  Lemke <tyleris@gmail.com>
2026-04-30 16:26:39 -0700

    multiple changes to form examples

---
+++ FS12-Week-7/Session-02/01-forms-validation/demo/script.js
@@ -0,0 +1,36 @@
+// Prevent Default allows us to override what we say in - demo-starter
+
+// How do we select the form
+const jobApplicationForm = document.querySelector("#job-application");
+
+// we need to attach an event listener to the form to override its
+// default submission action
+
+jobApplicationForm.addEventListener("submit", function (event) {
+  //Prevent the tradition form submission / page refresh
+  event.preventDefault();
+
+  /*
+    Typically here you would take all of the form data
+    and send it off in an API request to a server
+    and that data would then be stored for later retrieval in a database
+    we will learn how to do this soon
+  */
+
+  console.log("currentTarget", event.currentTarget);
+
+  // the form "currentTarget" has an elements object on it
+
+  console.log("form elements", event.currentTarget.elements);
+
+  // lets log out the experience input
+
+  console.log("experience input", event.currentTarget.elements.experience);
+
+  // to make it a little easier while we code we can collapse the . chaining down
+  // into a single
+  const formElements = event.currentTarget.elements;
+  const experienceInput = formElements.experience;
+
+  console.log("experience input value", experienceInput.value);
+});
