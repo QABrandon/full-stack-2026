e6e365e85e1f0876d9c8e94183257186838f4a04
Tyler  Lemke <tyleris@gmail.com>
2026-05-28 16:23:43 -0700

    changes

--- FS12-Week-011/Session-02/01-routing/02-express-routes-controllers-demo-start/index.js
+++ FS12-Week-011/Session-02/01-routing/02-express-routes-controllers-demo-start/index.js
@@ -1,48 +1,15 @@
+// Routes + Controllers Demo
+
 const express = require("express");
 const app = express();
+const userRoutes = require("./routes/users");
 
 //middleware which we will talk about in a bit
 app.use(express.json());
 
-//usually this data is in a Database
-let users = [
-  { id: 1, name: "Alice", email: "alice@example.com" },
-  { id: 2, name: "Bob", email: "bob@example.com" },
-];
-
-// GET All Users
-
-app.get("/users", (req, res) => {
-  // return back users and count in a new object
-  // when we visit localhost:3000/users
-  // you can do this in Postman/browser/whatever tool
-  res.json({
-    users: users,
-    count: users.length,
-  });
-});
-
-// Practice -
-// What would the route and code be for
-// GET by user id to get an individual user
-// app.get("/users/:id")
-
-// DELETE START - remove resource
-app.delete("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const userIndex = users.findIndex((u) => u.id === parseInt(id));
-
-  if (userIndex === -1) {
-    return res.status(404).json({ error: "User not found" });
-  }
-
-  const deletedUser = users.splice(userIndex, 1)[0];
-  res.json({
-    message: "User deleted",
-    user: deletedUser,
-  });
-});
-// DELETE END
+// get rid of the users implementation
+// then we have to add our routes
+app.use("/users", userRoutes);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
